67e13f166722c8e732c77df0b4eb0f3058b116fd
ExtraCrafTX <kaamilj@yahoo.com>
2016-10-04 16:20:42 +0100

    Updated comments for Exercise 3 (UndergroundZones)

--- src/UndergroundZones.java
+++ src/UndergroundZones.java
@@ -1,4 +1,9 @@
+/**
+ * This program asks the user for a tube station
+ * and tells them which zone it is in.
+ */
 
+//Imports scanner utility to allow user input
 import java.util.Scanner;
 
 /**
@@ -7,6 +12,8 @@ import java.util.Scanner;
  */
 public class UndergroundZones {
     
+    //Main method executed on launch. Calls the other methods in order to get
+    //input and calls the apropriate method based on the name that is input
     public static void main(String[] args) {
         String station = getInput("Which station would you like to know the zone of?").toLowerCase();
         if(station.contains("mile end")){
@@ -24,28 +31,34 @@ public class UndergroundZones {
         }
     }
     
+    //Gets input from the user
     public static String getInput(String message){
         Scanner scanner = new Scanner(System.in);
         System.out.println(message);
         return scanner.nextLine();
     }
     
+    //Returns the zone for Mile End
     public static int mileEnd(){
         return 2;
     }
     
+    //Returns the zone for Aldgate East
     public static int aldgateEast(){
         return 1;
     }
     
+    //Returns the zonr for Woodford
     public static int woodford(){
         return 4;
     }
     
+    //Returns the zone for West Harrow
     public static int westHarrow(){
         return 5;
     }
     
+    //Returns the zone for Highgate
     public static int highgate(){
         return 3;
     }
