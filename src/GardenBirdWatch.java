313044976a1fc4e6cedb5d4fd5a86c13eaf26679
ExtraCrafTX <kaamilj@yahoo.com>
2016-10-04 16:31:17 +0100

    Replaced literals with constants for Exercise 7 (GardenBirdWatch)

--- src/GardenBirdWatch.java
+++ src/GardenBirdWatch.java
@@ -13,6 +13,16 @@ import java.util.Scanner;
  */
 public class GardenBirdWatch {
     
+    public static final String END_CODE = "end";
+    public static final String NUM_TOO_SMALL = "Number must be greater than 0.";
+    public static final String NO_BIRDS = "You did not see any birds.";
+    public static final String MOST_COMMON_1 = "The ";
+    public static final String MOST_COMMON_2 = " was the most common bird seen at one time in your garden.";
+    public static final String HOW_MANY_1 = "You saw ";
+    public static final String HOW_MANY_2 = " of them at once.";
+    public static final String Q_WHICH_BIRD = "Which bird did you see?";
+    public static final String Q_HOW_MANY_BIRD = "How many did you see at once? (whole number > 0)";
+    
     /**
      * Called on launch. Calls various methods in order to run the program
      * @param args Command line arguments
@@ -25,11 +35,11 @@ public class GardenBirdWatch {
         //Gets input for the birds' name
         String input = getBirdName();
         //Loops while the user doesn't input the word 'end'
-        while(!input.toLowerCase().equals("end")){
+        while(!input.toLowerCase().equals(END_CODE)){
             int number = getBirdNumber();
             //Makes sure the input is valid, else loops again
             if(number <= 0){
-                System.out.println("Number must be greater than 0.");
+                System.out.println(NUM_TOO_SMALL);
                 System.out.println();
                 input = getBirdName();
                 continue;
@@ -44,7 +54,7 @@ public class GardenBirdWatch {
         }
         //Make sure some birds were seen, if not, say so
         if(maxNumber == 0){
-            System.out.println("You did not see any birds.");
+            System.out.println(NO_BIRDS);
         }
         //Outputs the bird seen the most
         else{
@@ -58,7 +68,7 @@ public class GardenBirdWatch {
      */
     public static String getBirdName(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Which bird did you see?");
+        System.out.println(Q_WHICH_BIRD);
         return scanner.nextLine();
     }
     
@@ -68,7 +78,7 @@ public class GardenBirdWatch {
      */
     public static int getBirdNumber(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("How many did you see at once? (whole number > 0)");
+        System.out.println(Q_HOW_MANY_BIRD);
         return scanner.nextInt();
     }
     
@@ -78,8 +88,8 @@ public class GardenBirdWatch {
      * @param number The number seen
      */
     public static void outputBirdMostSeen(String name, int number){
-        System.out.println("The " + name + " was the most common bird seen at one time in your garden.");
-        System.out.println("You saw " + number + " of them at once.");
+        System.out.println(MOST_COMMON_1 + name + MOST_COMMON_2);
+        System.out.println(HOW_MANY_1 + number + HOW_MANY_2);
     }
     
 }
